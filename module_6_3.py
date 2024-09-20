3d2a643253c0915d64c925b4ef20c8f44bc5f6e7
mik-305 <mik-305@bk.ru>
2024-09-20 16:39:39 +0300

    Update module_6_3.py

--- module_6_3.py
+++ module_6_3.py
@@ -1,18 +1,30 @@
 class Horse:
     x_distance = 0          # Пройденный путь
+    dx = 0
     sound = 'Frrr'          # Звук, издаваемый лошадью
     def run(self, dx):
         self.dx = x_distance + dx
 
 class Eagle:
     y_distance = 0          # высота полёта орла
+    dy = 0
     sound = 'I train, eat, sleep, and repeat' # Звук, издаваемый орлом
     def fly(self, dy):
         self.dy = y_distance + dy
 
 class Pegasus(Horse, Eagle):
+    #def __init__(self):
+
     def move(self, dx, dy):
+        self.dx = dx
+        self.dy = dy
 
-    def get_pos(self):
+    #def get_pos(self):
 
-    def voice(self):
+    def voice(self, sound):
+        self.sound = sound
+
+horse = Horse()
+eagle = Eagle()
+print(horse.sound)
+print(eagle.sound)
